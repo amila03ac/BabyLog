2ee816dc17d9360e8e8bf56f2811c3278b61d979
Amila Ariyarathna <amila@geveo.com>
2021-05-15 16:19:33 +0530

    Added event type controller

---
+++ Controllers/EventTypesController.cs
@@ -0,0 +1,44 @@
+﻿using BabyLog.Data;
+using BabyLog.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BabyLog.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class EventTypesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EventTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EventType>>> GetEventTypes()
+        {
+            return await _context.EventTypes.ToListAsync();
+        }
+
+        // GET: api/EventTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EventType>> GetEventType(int id)
+        {
+            var eventType = await _context.EventTypes.FindAsync(id);
+
+            if (eventType == null)
+            {
+                return NotFound();
+            }
+
+            return eventType;
+        }    
+    }
+}
